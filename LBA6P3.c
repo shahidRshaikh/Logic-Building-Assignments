21622ac7498267bf04013ec4a268bc3d1ec2726e
shahidRshaikh <shahidshaikh8049@gmail.com>
2025-06-04 11:32:30 +0530

    Update LBA6P3.c

--- LBA6P3.c
+++ LBA6P3.c
@@ -4,7 +4,7 @@
 //
 //  File Namne :   LBA6P3.c
 //  Description :  write a  program to find factorial of given number
-//  Author :       Omkar Santosh Wagh
+//  Author :        Shahid Rajjak Shaikh
 //  Date :         14/05/2025
 //
 /////////////////////////////////////////////////////////////////////////
