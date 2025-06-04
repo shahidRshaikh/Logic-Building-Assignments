e6d3336de328993766e248d3eff64e2cd248e51b
shahidRshaikh <shahidshaikh8049@gmail.com>
2025-06-04 12:42:55 +0530

    Update LBA18P2.c

--- LBA18P2.c
+++ LBA18P2.c
@@ -12,7 +12,7 @@
 // 
 //  File Name :     LBA18P2.c
 //  Description :   Accept N number from user and display all such elemnts which are Divisible by 5.
-//  Author :        Omkar Santosh Wagh
+//  Author :        Shahid Rajjak Shaikh
 //  Date :          03/06/2025
 // 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,7 +27,7 @@
 //  Input Arguments : Integer array (Arr) - contains N elements
 //                    Integer (iSize) - number of elements in the array
 //  Output          : Display the elements divisible by 5.
-//  Author          : Omkar Santosh Wagh
+//  Author          : Shahid Rajjak Shaikh
 //  Date            : 03/06/2025
 //
 //////////////////////////////////////////////////////////////////////////////////////////////
