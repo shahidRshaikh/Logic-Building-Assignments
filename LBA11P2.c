b888cf2cd89d49fadb8bbd0f9c07548870ad7dcf
shahidRshaikh <shahidshaikh8049@gmail.com>
2025-06-04 11:58:57 +0530

    Update LBA11P2.c

--- LBA11P2.c
+++ LBA11P2.c
@@ -1,20 +1,54 @@
-// Input : 5
-// Output : 5 # 4 # 3 # 2 # 1# 
+// Accept number from user and Display below pattern
+// Input   :  5
+// Output  :  5     #      4      #     3      #    2   #   1   #
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File Name :     LBA11P2.c
+//  Description :   Accept number from user and Display pattern
+//  Author :        Shahid Rajjak Shaikh
+//  Date :          02/06/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :      Pattern
+//  Description  :       Accept number from user and Display pattern
+//  Input Arguments :    Integer
+//  Output :             Integer, Special Symbol
+//  Author :             Shahid Rajjak Shaikh 
+//  Date :               02/06/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 void Pattern(int iNo)
 {
-    for(int i = 0; i >= iNo; i--)
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt > 0; iCnt--)
     {
-        printf("%d #", i);
+        printf("%d\t#\t",iCnt);
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Entry Point Function
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
 int main()
 {
     int iValue = 0;
-    printf("Enter number of elements :");
-    scanf("%d %d",&iValue);
+
+    printf("Enter the Number : \n");
+    scanf("%d",&iValue);
 
     Pattern(iValue);
+
     return 0;
 }
